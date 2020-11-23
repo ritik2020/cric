dee2cd6d82f7e7144d371486ee2b774b0ed9b4ae
Rohan Jha <yorohan.jha@gmail.com>
2020-11-23 12:09:08 +0530

    Added batting order and created utility fns

--- src/app/team-analysis/team-analysis.component.ts
+++ src/app/team-analysis/team-analysis.component.ts
@@ -12,8 +12,8 @@ export class TeamAnalysisComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.wicketData = this.wicketsDetailOfAMatch(3,2);
-    this.wicketDetailsOfAllMatch();
+    this.wicketData = this.wicketsDetailOfAMatch(3);
+    this.battingOrderOfAMatch(0);
   }
 
   formatDelivery(delivery){
@@ -26,22 +26,23 @@ export class TeamAnalysisComponent implements OnInit {
     let res = [];
     for(let i=0; i<this.matchesData.length; i++){
       if(this.matchesData[i].innings[0]["1st innings"].team === this.selectedTeam){
-        res.push(this.wicketsDetailOfAMatch(i+1,1));
+        res.push(this.wicketsDetailOfAMatch(i));
       }
       if(this.matchesData[i].innings[1]["2nd innings"].team === this.selectedTeam){
-        res.push(this.wicketsDetailOfAMatch(i+1,2));
+        res.push(this.wicketsDetailOfAMatch(i));
       }
     }
-    console.log(res);
+    return res;
   }
   
-  wicketsDetailOfAMatch(match, inning){
+  wicketsDetailOfAMatch(match){
+    let inning = this.getBattingInnings(match);
     var deliveries;
-    if(inning===1){
-      deliveries = this.matchesData[match-1].innings[inning-1]["1st innings"].deliveries;
+    if(inning===0){
+      deliveries = this.matchesData[match].innings[inning]["1st innings"].deliveries;
     }
-    if(inning===2){
-      deliveries = this.matchesData[match-1].innings[inning-1]["2nd innings"].deliveries;
+    if(inning===1){
+      deliveries = this.matchesData[match].innings[inning]["2nd innings"].deliveries;
     }
     var res = [];
     for(let i=0; i<deliveries.length; i++){
@@ -60,4 +61,86 @@ export class TeamAnalysisComponent implements OnInit {
     return res;
   }
   
+  battingOrderOfAMatch(match){
+    let wicketsData = this.wicketsDetailOfAMatch(match);
+    let wicketDeliveries = [];
+    let res = [];
+    wicketsData.forEach(wicket=>{
+      wicketDeliveries.push(wicket.delivery);
+    });
+
+    let openers = this.getDeliveryData(match,this.getBattingInnings(match),"0.1");
+    let visitedBatsman = [];
+    res.push({batsman: openers.batsman, over:"0.1"});
+    visitedBatsman.push(openers.batsman);
+    res.push({batsman: openers.non_striker, over:"0.1"});
+    visitedBatsman.push(openers.non_striker);
+
+    for(let i=0; i<wicketDeliveries.length; i++){
+      let d = (Number(wicketDeliveries[i])+0.1).toFixed(1);
+      let deliveryData= this.getDeliveryData(match,this.getBattingInnings(match),d);
+      if(!visitedBatsman.includes(deliveryData.batsman)){
+        res.push({batsman: deliveryData.batsman, over: d});
+        visitedBatsman.push(deliveryData.batsman);
+      }
+      else{
+        res.push({batsman: deliveryData.non_striker, over: d});
+        visitedBatsman.push(deliveryData.non_striker);
+      }
+    }
+
+    console.log(res);
+  }
+
+  getDate(match){
+    return this.matchesData[match].info.dates[0];
+  }
+
+  getWinner(match){
+    return this.matchesData[match].info.outcome.winner;
+  }
+
+  getPlayerOfAMatch(match){
+    return this.matchesData[match].info.player_of_match;
+  }
+
+  getOpponent(match){
+    let teams = this.matchesData[match].info.teams;
+    if(teams[0]!=this.selectedTeam){
+      return teams[0];
+    }
+    else{
+      return teams[1];
+    }
+  }
+
+  getVenue(match){
+    return this.matchesData[match].info.venue;
+  }
+
+  getBattingInnings(match){
+    if(this.matchesData[match].innings[0]["1st innings"].team === this.selectedTeam){
+      return 0;
+    }
+    else{
+      return 1;
+    }
+  }
+
+  getDeliveryData(match,inning,delivery){
+    let deliveries;
+    if(inning===0){
+      deliveries = this.matchesData[match].innings[inning]["1st innings"].deliveries;
+    }
+    else {
+      deliveries = this.matchesData[match].innings[inning]["2nd innings"].deliveries;
+    }
+
+    for(let i=0; i<deliveries.length; i++){
+      let del = Object.keys(deliveries[i])[0];
+      if(del===delivery){
+        return deliveries[i][del];
+      }
+    }
+  }
 }
